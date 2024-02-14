f296f28f549c1fd43cc2b67d60de6959e3678df6
JO WONYOUNG <118702713+WONYOUNG-HC@users.noreply.github.com>
2024-02-14 20:21:01 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 24 ms, Memory: 7996 KB -BaekjoonHub

---
+++ 백준/Gold III/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+char arr[1010][1010];
+int vis[1010][1010];
+bool safe[1010][1010];
+
+void dfs(int y, int x) {
+    int ny = y, nx = x;
+    if (arr[y][x] == 'U') 
+        ny -= 1;
+    else if (arr[y][x] == 'D') 
+        ny += 1;
+    else if (arr[y][x] == 'L') 
+        nx -= 1;
+    else 
+        nx += 1;
+
+    if (vis[ny][nx] == 0) {
+        vis[ny][nx] = 1;
+        dfs(ny, nx);
+        vis[ny][nx] = 2;
+    }
+    else if (vis[ny][nx] == 1) {
+        safe[ny][nx] = true;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++) 
+            if(vis[i][j] == 0)
+                dfs(i, j);
+        
+    int ans = 0;
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++)
+            safe[i][j] && ans++;
+    
+    cout << ans;
+}
